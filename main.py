e5951f7ee850c438284eb089db99d96ffb1143f5
Javo205 <javier.decastro24@gmail.com>
2025-03-09 14:23:31 +0100

    Comments added

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from simulation import Simulation
 
 # TODO: Document project and write README
 if __name__ == "__main__":
-    sim = Simulation("config.json")
-    sim.save_animation("output.mp4", fps=30)  # Save animation instead of displaying
+    sim = Simulation("config.json")  # Initialization of Simulation object
+    sim.save_animation("output.mp4", fps=30)  # Save animation
